04b57a6273b0a38c265ab800eadd8ceaf33e8ef4
Coder Sourya <thecreativeworldforall@gmail.com>
2023-04-17 21:36:46 +0530

    Restructure

--- src/Deduction/DeductionBy80C.js
+++ src/Deduction/DeductionBy80C.js
@@ -1,77 +1,91 @@
-
 import React, { useRef, useState } from "react";
-import {validateEntry,convertEntry,calculateTotalDeduction} from './DeductionLogic'
-import Input from "../Generic/Input/Input"
+// components
+import {
+  validateEntry,
+  convertEntry,
+  calculateTotalDeduction,
+} from "./DeductionLogic";
+import Input from "../Generic/Input/Input";
 import AddRemoveDeduction from "./AddRemoveDeduction";
 
-const MAX_80_C_LIMIT = 150000
+const MAX_80_C_LIMIT = 150000;
 const DeductionBy80C = (props) => {
+  const pfAmount = useRef();
+  const licDeduction = useRef();
+  const loanDeduction = useRef();
+  const otherDeduction = useRef();
 
-    const pfAmount = useRef();
-    const licDeduction = useRef();
-    const loanDeduction = useRef();
-    const otherDeduction = useRef();
- 
-    const isIncluded = props.amount > 0 ? true : false
- 
-    const [errorMsg, setErrorMsg] = useState('')
- 
-    /* const calculateTotalDeduction = (details) => {
+  const isIncluded = props.amount > 0 ? true : false;
+
+  const [errorMsg, setErrorMsg] = useState("");
+
+  /* const calculateTotalDeduction = (details) => {
         let totalDeduction = 0
         for (const key in details) {
             totalDeduction += details[key]
         }
         return totalDeduction > MAX_80_C_LIMIT ? MAX_80_C_LIMIT : totalDeduction
     }  */
-    
-    const clearEntry = () => {
-        pfAmount.current.value = ''
-        licDeduction.current.value = ''
-        loanDeduction.current.value = ''
-        otherDeduction.current.value = ''
+
+  const clearEntry = () => {
+    pfAmount.current.value = "";
+    licDeduction.current.value = "";
+    loanDeduction.current.value = "";
+    otherDeduction.current.value = "";
+  };
+
+  const addDeduction = () => {
+    const deductionDetails = {
+      pf: pfAmount.current.value,
+      lic: licDeduction.current.value,
+      homeLoan: loanDeduction.current.value,
+      other: otherDeduction.current.value,
+    };
+    if (validateEntry(deductionDetails)) {
+      const totalDeduction = calculateTotalDeduction(
+        convertEntry(deductionDetails),
+        MAX_80_C_LIMIT
+      );
+      if (totalDeduction === 0) {
+        setErrorMsg("Enter at least one");
+      }
+
+      if (totalDeduction > 0) {
+        setErrorMsg("");
+        props.onDeduction(totalDeduction);
+        clearEntry();
+      }
+    } else {
+      setErrorMsg("Enter Numeric values only");
     }
+  };
 
-    const addDeduction = () => {
-        const deductionDetails = {
-            pf: pfAmount.current.value,
-            lic: licDeduction.current.value,
-            homeLoan: loanDeduction.current.value,
-            other: otherDeduction.current.value
-        }
-        if (validateEntry(deductionDetails)) {
-            const totalDeduction = calculateTotalDeduction(convertEntry(deductionDetails),MAX_80_C_LIMIT)
-            if (totalDeduction === 0) {
-                setErrorMsg('Enter at least one')
-            }
+  const removeDeduction = () => {
+    props.onDeduction(0);
+  };
+  return (
+    <div>
+      <header>
+        {isIncluded && <span>&#9989;</span>}
+        DEDUCTION UNDER 80C [ Max Limit Rs.{MAX_80_C_LIMIT} ] [YEARLY]
+      </header>
 
-            if (totalDeduction > 0) {
-                setErrorMsg('')
-                props.onDeduction(totalDeduction)
-                clearEntry() 
-            }
-        } else {
-            setErrorMsg('Enter Numeric values only')
-        }
-    }
+      {errorMsg !== "" && <p className="error-msg">* {errorMsg}</p>}
+      <Input label="Provident Fund" ref={pfAmount} errorMsg="" />
+      <Input label="LIC" ref={licDeduction} />
+      <Input label="Home Loan Principal Repay" ref={loanDeduction} />
+      <Input
+        label="Fxed Deposit / NSC / VPF / PPF / Other"
+        ref={otherDeduction}
+      />
 
-    const removeDeduction = () => {
-        props.onDeduction(0) 
-    }
-    return (
-        <div>
-            <header>
-                {isIncluded && <span>&#9989;</span>}
-                DEDUCTION UNDER 80C [ Max Limit Rs.{MAX_80_C_LIMIT} ] [YEARLY]</header>
+      <AddRemoveDeduction
+        isAdded={isIncluded}
+        onAdd={addDeduction}
+        onRemove={removeDeduction}
+      />
+    </div>
+  );
+};
 
-            {errorMsg !== '' && <p className="error-msg">* {errorMsg}</p>}
-            <Input label='Provident Fund' ref={pfAmount} errorMsg='' />
-            <Input label='LIC' ref={licDeduction} />
-            <Input label='Home Loan Principal Repay' ref={loanDeduction} />
-            <Input label='Fxed Deposit / NSC / VPF / PPF / Other' ref={otherDeduction} />
-
-            <AddRemoveDeduction isAdded={isIncluded} onAdd={addDeduction} onRemove={removeDeduction} />
-        </div>
-    )
-}
-
-export default DeductionBy80C
+export default DeductionBy80C;
